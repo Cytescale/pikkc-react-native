c26e95a4bed905ddc563709a4878746d9ddd0b71
cytescale <nikhil@cytescale.com>
2021-07-16 20:59:03 +0530

    Init pikkc build

--- App.js
+++ App.js
@@ -2,20 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import LandAct from './src/land';
+import LoginAct from './src/login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen  options={{headerShown: false}} name="login" component={LoginAct} />
+        <Stack.Screen options={{headerShown: false}}  name="land" component={LandAct} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
